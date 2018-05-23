5b7f692f55b1d55bfea46b8f405396fecd88c85e
Andrew Williams <Andrew.Williams@curtin.edu.au>
2018-05-23 10:38:50 +0800

    bugfix

--- mwa_trigger/fermi_swift.py
+++ mwa_trigger/fermi_swift.py
@@ -314,7 +314,7 @@ def handle_grb(v, pretend=False):
         clear_data = "Commands:\n%s \n\n STDOUT:\n%s \n\n STDERR:\n%s" % (result['clear']['command'],
                                                                           result['clear']['stdout'],
                                                                           result['clear']['stderr'])
-        log_data = '\n'.join(grb.loglist)
+        log_data = '\n'.join([str(x) for x in grb.loglist])
         handlers.send_email(from_address='mwa@telemetry.mwa128t.org',
                             to_addresses=NOTIFY_LIST,
                             subject=EMAIL_SUBJECT_TEMPLATE % trig_id,
