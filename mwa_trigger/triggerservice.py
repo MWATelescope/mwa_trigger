1a0686725e3fa26bea5b1480159403e4e0f112fe
Andrew Williams <Andrew.Williams@curtin.edu.au>
2018-09-17 11:21:33 +0800

    Add triggerbuffer service to the trigger handler, to force a buffer dump to disk if running in VOLTAGE_BUFFER mode.

--- mwa_trigger/triggerservice.py
+++ mwa_trigger/triggerservice.py
@@ -90,10 +90,10 @@ def web_api(url='', urldict=None, postdict=None, username=None, password=None, l
 
 def busy(project_id=None, obstime=None, logger=DEFAULTLOGGER):
     """
-    Call with a project_id and a desired observing time. This function will return True if the given project_id
-    is allowed to override current observations from now for the given length of time, or False if not.
+    Call with a project_id and a desired observing time. This function will return False if the given project_id
+    is allowed to override current observations from now for the given length of time, or True if not.
 
-    Note that this doesn't guarantee a later call to trigger() will succeed, as new observations may have been
+    Note that a False result doesn't guarantee a later call to trigger() will succeed, as new observations may have been
     added to the schedule in the meantime.
 
     :param project_id: eg 'C001'
@@ -259,4 +259,40 @@ def trigger(project_id=None, secure_key=None,
         result = web_api(url=BASEURL + 'triggervcs', urldict=urldict, postdict=postdict, logger=logger)
     else:
         result = web_api(url=BASEURL + 'triggerobs', urldict=urldict, postdict=postdict, logger=logger)
+    return result
+
+
+def triggerbuffer(project_id=None,
+                  secure_key=None,
+                  pretend=None,
+                  logger=DEFAULTLOGGER):
+    """
+    Assuming the correlator is in VOLTAGE_BUFFER mode, trigger an immediate dump of the memory buffers to
+    disk, and continue capturing voltage data until either the disks are full, or a STOP_CAPTURE observation
+    is processed.
+
+    :param project_id: eg 'C001' - project ID for the triggered observations
+    :param secure_key: password associated with that project_id
+    :param pretend: boolean or integer. If True, the triggervcs command will NOT be run.
+    :param logger: optional logging.logger object
+    :return: A tuple of (success, message) where success is a boolean, and message is a response string.
+    """
+    urldict = {}
+    postdict = {}
+    if project_id is not None:
+        urldict['project_id'] = project_id
+    else:
+        logger.error('triggering.trigger() must be passed a valid project_id')
+        return None
+
+    if secure_key is not None:
+        postdict['secure_key'] = secure_key
+    else:
+        logger.error('triggering.trigger() must be passed a valid secure_key')
+        return None
+
+    if pretend is not None:
+        postdict['pretend'] = pretend
+
+    result = web_api(url=BASEURL + 'triggerbuffer', urldict=urldict, postdict=postdict, logger=logger)
     return result
