d5d31fced5ee45f1dd0be65305fe99db24f8c9bf
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2019-05-17 14:14:08 +1000

    Only test whether time since merger exceeds max response time if the event is not already currently being observed.

--- mwa_trigger/GW_LIGO.py
+++ mwa_trigger/GW_LIGO.py
@@ -538,22 +538,7 @@ def handle_gw(v, pretend=False, time=None):
 #                            attachments=[('voevent.xml', voeventparse.dumps(v))])
 #        return
 
-    time_string = v.WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Time.TimeInstant.ISOTime.text
     
-    merger_time = Time(time_string)
-    delta_T = Time.now() - merger_time
-    delta_T_sec = delta_T.sec
-    
-    if delta_T_sec > MAX_RESPONSE_TIME:
-        log_message = "Time since merger (%d s) greater than max response time (%d s). Not triggering" % (delta_T_sec, MAX_RESPONSE_TIME)
-        log.info(log_message)
-        handlers.send_email(from_address='mwa@telemetry.mwa128t.org',
-                            to_addresses=DEBUG_NOTIFY_LIST,
-                            subject=debug_email_subject,
-                            msg_text=DEBUG_EMAIL_TEMPLATE % log_message,
-                            attachments=[('voevent.xml', voeventparse.dumps(v))])
-                            
-        return
 
 #    if params['Group'] != 'CBC':
 #        log.debug("Event not CBC")
@@ -603,6 +588,7 @@ def handle_gw(v, pretend=False, time=None):
 
     obslist = triggerservice.obslist(obstime=req_time_s)
 
+    currently_observing = False
     if obslist is not None and len(obslist) > 0:
         gw.debug("Currently observing:")
         gw.debug(str(obslist))
@@ -611,6 +597,7 @@ def handle_gw(v, pretend=False, time=None):
         gw.debug("obs {0}, trig {1}".format(obs, trig_id))
         
         if obs == trig_id:
+            currently_observing = True
             gw.info("Already observing this GW event")
             
             last_pos = gw.get_pos(-2)
@@ -629,8 +616,29 @@ def handle_gw(v, pretend=False, time=None):
             
             else:
               gw.info("Updating pointing.")
+        
+    if not currently_observing:
+        #If this event is not currently being observed, check whether time since merger exceeds max response time
+        time_string = v.WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Time.TimeInstant.ISOTime.text
 
+        merger_time = Time(time_string)
+        delta_T = Time.now() - merger_time
+        delta_T_sec = delta_T.sec
+        
+        if delta_T_sec > MAX_RESPONSE_TIME:
+            log_message = "Time since merger (%d s) greater than max response time (%d s). Not triggering" % (delta_T_sec, MAX_RESPONSE_TIME)
+            log.info(log_message)
+            handlers.send_email(from_address='mwa@telemetry.mwa128t.org',
+                                to_addresses=DEBUG_NOTIFY_LIST,
+                                subject=debug_email_subject,
+                                msg_text=DEBUG_EMAIL_TEMPLATE % log_message,
+                                attachments=[('voevent.xml', voeventparse.dumps(v))])
+                                
+            return
+        
+    #Check if this event has been triggered on before
     if gw.first_trig_time is not None:
+        #If it has been triggered, update the required time for the updated observation
         req_time_s -= (Time.now()-gw.first_trig_time).sec
         gw.info("Required observing time: %.0f s"%(req_time_s))
 
