1eeb03a29e0c6b428372e828f694afb6b6bb12c4
Andrew Williams <Andrew.Williams@curtin.edu.au>
2020-02-28 11:02:00 +0800

    Hack SWIFT handler to force 5 minute VCS observation instead of 30 minutes, until 2020-03-16

--- mwa_trigger/GRB_fermi_swift.py
+++ mwa_trigger/GRB_fermi_swift.py
@@ -454,6 +454,11 @@ def handle_grb(v, pretend=False):
                  'err': grb.err[-1]}
     email_text = EMAIL_TEMPLATE % emaildict
     email_subject = EMAIL_SUBJECT_TEMPLATE % grb.trigger_id
+
+    # Hack to drop the requested VCS time to 5 minutes for the last few weeks of 2019B
+    if grb.vcsmode:
+        req_time_min = 5.0   # TODO - remove this hack after 2020-03-16
+
     # Do the trigger
     result = grb.trigger_observation(ttype=this_trig_type,
                                      obsname=trig_id,
