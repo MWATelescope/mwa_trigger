da082159ca2ecf06267ac46434226ef38e5f25da
Nick <nickaswainston@gmail.com>
2022-01-20 09:50:50 +0800

    Declared media and static dirs in settings

--- trigger_webapp/trigger_webapp/settings.py
+++ trigger_webapp/trigger_webapp/settings.py
@@ -125,6 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
