3b08676b517102cb9d144935ee5d1ba150c6b638
Nick <nickaswainston@gmail.com>
2022-01-20 09:36:45 +0800

    Add the ability to handle Antares triggers and get sequence numbers from Fermi

--- mwa_trigger/parse_xml.py
+++ mwa_trigger/parse_xml.py
@@ -6,26 +6,36 @@ logger = logging.getLogger(__name__)
 
 
 def get_telescope(ivorn):
-    trig_swift = ("ivo://nasa.gsfc.gcn/SWIFT#BAT_GRB_Pos",  # Swift positions
-                    )
-
-    # Ignore "ivo://nasa.gsfc.gcn/Fermi#GBM_Alert" as they always have ra/dec = 0/0
-    trig_fermi = ("ivo://nasa.gsfc.gcn/Fermi#GBM_Flt_Pos",  # Fermi positions
-                    "ivo://nasa.gsfc.gcn/Fermi#GBM_Gnd_Pos",
-                    "ivo://nasa.gsfc.gcn/Fermi#GBM_Fin_Pos",
-                    )
-
+    # Check for SWIFT triggers
+    trig_swift = ("ivo://nasa.gsfc.gcn/SWIFT#BAT_GRB_Pos",
+                 )
     for t in trig_swift:
         if ivorn.startswith(t):
             return 'SWIFT'
+
+    # Check for Fermi triggers
+    # Ignore "ivo://nasa.gsfc.gcn/Fermi#GBM_Alert" as they always have ra/dec = 0/0
+    trig_fermi = ("ivo://nasa.gsfc.gcn/Fermi#GBM_Flt_Pos",
+                  "ivo://nasa.gsfc.gcn/Fermi#GBM_Gnd_Pos",
+                  "ivo://nasa.gsfc.gcn/Fermi#GBM_Fin_Pos",
+                 )
     for t in trig_fermi:
         if ivorn.startswith(t):
             return 'Fermi'
+
+    # Check for Antares triggers
+    trig_ant = ("ivo://nasa.gsfc.gcn/Antares_Alert#",
+               )
+    for t in trig_ant:
+        if ivorn.startswith(t):
+            return 'Antares'
+
+
     # Not found so return None
     return None
 
 
-class Trigger_Event:
+class VOEvent:
     def __init__(self, xml, packet=None):
         self.xml = xml
         self.packet = packet
@@ -45,15 +55,24 @@ class Trigger_Event:
         # Parse trigger info (telescope dependent)
         if telescope == 'Fermi':
             self.trig_time = float(v.find(".//Param[@name='Trig_Timescale']").attrib['value'])
-            self.trig_id = "Fermi_" + v.attrib['ivorn'].split('_')[-2]
             self.this_trig_type = v.attrib['ivorn'].split('_')[1]  # Flt, Gnd, or Fin
+            self.sequence_num = int(v.find(".//Param[@name='Sequence_Num']").attrib['value'])
         elif telescope == 'SWIFT':
             self.trig_time = float(v.find(".//Param[@name='Integ_Time']").attrib['value'])
-            self.trig_id = "SWIFT_" + v.attrib['ivorn'].split('_')[-1].split('-')[0]
             self.this_trig_type = "SWIFT"
+            self.sequence_num = None
+        elif telescope == 'Antares':
+            self.trig_time = None
+            self.this_trig_type = 'Antares'
+            self.sequence_num = None
+
+
+        #print(voeventparse.prettystr(v.What))
+        self.trig_id = int(v.find(".//Param[@name='TrigID']").attrib['value'])
         logger.debug("Trig details:")
         logger.debug(f"Dur:  {self.trig_time} s")
         logger.debug(f"ID:   {self.trig_id}")
+        logger.debug(f"Seq#: {self.sequence_num}")
         logger.debug(f"Type: {self.this_trig_type}")
 
         # Get current position
