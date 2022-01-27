ac944ad87d5cf0124b04507366cc35cd4d73d833
Nick Swainston <nicholaswainston@gmail.com>
2022-01-27 10:27:26 +0800

    Made a simple login page for non admins

--- trigger_webapp/trigger_webapp/settings.py
+++ trigger_webapp/trigger_webapp/settings.py
@@ -106,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
--- trigger_webapp/trigger_webapp/urls.py
+++ trigger_webapp/trigger_webapp/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from trigger_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home_page),
     path('trigger_log/', views.TriggerEventList.as_view()),
     path('voevent_log/', views.VOEventList.as_view()),
