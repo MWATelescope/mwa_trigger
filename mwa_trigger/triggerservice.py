dfa99c86c1e984aed6a3b8136ce9e97c045d4db1
Nick Swainston <nicholas.swainston@curtin.edu.au>
2022-02-02 10:05:15 +0800

    Refixed the triggerservice that I think was undone in a merge

--- mwa_trigger/triggerservice.py
+++ mwa_trigger/triggerservice.py
@@ -70,6 +70,8 @@ def web_api(
 
     if postdict is not None:
         postdata = urlencode(postdict)
+        if sys.version_info.major > 2:
+            postdata = postdata.encode()
     else:
         postdata = None
 
@@ -80,31 +82,23 @@ def web_api(
     logger.debug("Request: %s %s." % (reqtype, url))
     if postdict:
         logger.debug("Data: %s" % postdict)
-    try:
-        if (username is not None) and (password is not None):
-            if sys.version_info.major > 2:
-                base64string = base64.b64encode(
-                    ("%s:%s" % (username, password)).encode("latin-1")
-                )
-                base64string = base64string.decode("latin-1")
-                postdata = postdata.encode("latin-1")
-            else:
-                base64string = base64.b64encode("%s:%s" % (username, password))
-            req = Request(
-                url,
-                postdata,
-                {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": "Basic %s" % base64string,
-                },
+
+    # Set up request
+    header = {"Content-Type": "application/json",
+              "Accept": "application/json",
+    }
+    if (username is not None) and (password is not None):
+        # Add authenticatin
+        if sys.version_info.major > 2:
+            base64string = base64.b64encode(
+                ("%s:%s" % (username, password)).encode()
             )
+            base64string = base64string.decode()
         else:
-            req = Request(
-                url,
-                postdata,
-                {"Content-Type": "application/json", "Accept": "application/json"},
-            )
+            base64string = base64.b64encode("%s:%s" % (username, password))
+        header["Authorization"] = "Basic %s" % base64string
+    try:
+        req = Request(url, postdata, header)
         try:
             resobj = urlopen(req)
             data = resobj.read()
