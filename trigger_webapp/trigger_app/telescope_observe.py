aee29fe8dfdff7f08ca9b291f5c0ca682db7b4e6
Nick Swainston <nicholas.swainston@curtin.edu.au>
2022-02-14 11:25:25 +0800

    Now uses the project settings when scheduling MWA observations

--- trigger_webapp/trigger_app/telescope_observe.py
+++ trigger_webapp/trigger_app/telescope_observe.py
@@ -1,9 +1,9 @@
 import os
-from astropy.units import deg
-from astropy.coordinates import SkyCoord, EarthLocation
+import astropy.units as u
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astropy.time import Time
 
-from mwa_trigger.triggerservice import trigger
+from mwa_trigger.triggerservice import trigger_mwa
 from .models import Observations, VOEvent
 
 import logging
@@ -14,6 +14,32 @@ def trigger_observation(project_decision_model,
                         reason="First Observation"):
     """Wrap the differente observation functions
     """
+    # Check if source is above the horizon
+    # Create Earth location for MWA
+    # TODO ADD ONE FOR ATCA
+    location = EarthLocation(
+        lon=116.671*u.deg,
+        lat=-26.7033*u.deg,
+        height=377.827*u.m
+    )
+    print(location)
+    obs_source = SkyCoord(
+        project_decision_model.ra,
+        project_decision_model.dec,
+        #equinox='J2000',
+        unit=(u.deg, u.deg)
+    )
+    print(obs_source)
+    # Convert from RA/Dec to Alt/Az
+    obs_source_altaz = obs_source.transform_to(AltAz(obstime=Time.now(), location=location))
+    alt = obs_source_altaz.alt.deg
+    logger.debug("Triggered observation at an elevation of {0}".format(alt))
+    if alt < project_decision_model.project.horizon_limit:
+        horizon_message = f"Not triggering due to horizon limit: alt {alt} < {project_decision_model.project.horizon_limit}. "
+        logger.debug(horizon_message)
+        return 'I', trigger_message + horizon_message
+
+    # above the horizon so send off telescope specific set ups
     if project_decision_model.project.telescope.startswith("MWA"):
         # If telescope ends in VCS then this project is for observing in VCS mode
         vcsmode = project_decision_model.project.telescope.endswith("VCS")
@@ -38,31 +64,20 @@ def trigger_observation(project_decision_model,
 
 def trigger_mwa_observation(project_decision_model,
                             trigger_message,
-                            horizion_limit=30,
-                            pretend=True,
                             vcsmode=False):
     """Check if the mwa can observe then send it off the observation.
     """
-    # set up the target, observer, and time
-    obs_source = SkyCoord(ra=project_decision_model.ra,
-                          dec=project_decision_model.dec,
-                          equinox='J2000',
-                          unit=(deg, deg))
-    obs_source.location = EarthLocation.from_geodetic(lon="116:40:14.93",
-                                                      lat="-26:42:11.95",
-                                                      height=377.8)
-    t = Time.now()
-    obs_source.obstime = t
+    proj_settings = project_decision_model.project
 
-    # figure out the altitude of the target
-    obs_source_altaz = obs_source.transform_to('altaz')
-    alt = obs_source_altaz.alt.deg
-    logger.debug("Triggered observation at an elevation of {0}".format(alt))
-
-    if alt < project_decision_model.project.horizon_limit:
-        horizon_message = f"Not triggering due to horizon limit: alt {alt} < {project_decision_model.project.horizon_limit}. "
-        logger.debug(horizon_message)
-        return 'I', trigger_message + horizon_message, []
+    # Calculate number of obs and their duration
+    if vcsmode:
+        # VCS mode uses a single observation only
+        nobs = 1
+        exptime = proj_settings.mwa_exptime
+    else:
+        # normal observations split this time into 2 min chunks
+        nobs = int(proj_settings.mwa_exptime / 120)
+        exptime = 120
 
     # Collect event telescopes
     voevents = VOEvent.objects.filter(trigger_group_id=project_decision_model.trigger_group_id)
@@ -73,24 +88,24 @@ def trigger_mwa_observation(project_decision_model,
     telescopes = "_".join(list(set(telescopes)))
 
     # Not below horizon limit so observer
-    logger.info(f"Triggering at gps time {t.gps} ...")
-    result = trigger_mwa(project_id='C002',
+    logger.info(f"Triggering at gps time {Time.now()} ...")
+    result = trigger_mwa(project_id=proj_settings.project_id,
         secure_key=os.environ['MWA_SECURE_KEY'],
-        group_id=project_decision_model.trigger_group_id.trigger_id,
-        pretend=project_decision_model.project.testing,
+        #group_id=project_decision_model.trigger_group_id.trigger_id, # only need this for follow up obs
+        pretend=proj_settings.testing,
         ra=project_decision_model.ra, dec=project_decision_model.dec,
         creator='VOEvent_Auto_Trigger', #TODO grab version
         obsname=f'{telescopes}_{project_decision_model.trigger_group_id.trigger_id}',
-        nobs=1, # Changes if not in VCS
-        freqspecs='145,24',
-        avoidsun=True,
-        inttime=0.5,
-        freqres=10,
-        exptime=15, # TODO (Default VCS time) change this for non vcs observing
-        calibrator=True,
-        calexptime=120,
+        nobs=nobs,
+        freqspecs=f"{proj_settings.mwa_centrefreq},24", #Assume always using 24 contiguous coarse frequency channels
+        avoidsun=proj_settings.mwa_avoidsun,
+        inttime=proj_settings.mwa_inttime,
+        freqres=proj_settings.mwa_freqres,
+        exptime=exptime,
+        calibrator=proj_settings.mwa_calibrator,
+        calexptime=proj_settings.mwa_calexptime,
         vcsmode=vcsmode,
-        buffered=False,
+        buffered=proj_settings.mwa_buffered,
     )
     # Check if succesful
     if not result['success']:
