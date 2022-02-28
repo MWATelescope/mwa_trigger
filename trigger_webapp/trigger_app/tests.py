bb6ad4990cd554d0150ccf7403b0d29a718cac13
Nick Swainston <nicholas.swainston@curtin.edu.au>
2022-02-28 09:53:12 +0800

    Made the test events aalways at zenith and a test that the proposal decision is to trigger

--- trigger_webapp/trigger_app/tests.py
+++ trigger_webapp/trigger_app/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 
-from .models import VOEvent, TriggerEvent, ProposalSettings
+from .models import VOEvent, TriggerEvent, ProposalDecision
 
 from mwa_trigger.parse_xml import parsed_VOEvent
-from astropy.coordinates import Angle
 import astropy.units as u
+from astropy.coordinates import Angle, SkyCoord, EarthLocation
+from astropy.time import Time
 import datetime
 
 class test_group_01(TestCase):
@@ -16,6 +17,14 @@ class test_group_01(TestCase):
             "../tests/test_events/group_01_02_Fermi.xml",
             "../tests/test_events/group_01_03_SWIFT.xml"
         ]
+
+        # Setup current RA and Dec at zenith for the MWA
+        MWA = EarthLocation(lat='-26:42:11.95', lon='116:40:14.93', height=377.8 * u.m)
+        mwa_coord = coord = SkyCoord(az=0., alt=90., unit=(u.deg, u.deg), frame='altaz', obstime=Time.now(), location=MWA)
+        ra_dec = mwa_coord.icrs
+        ra_deg = ra_dec.ra.deg
+        dec_deg = ra_dec.dec.deg
+
         # Parse and upload the xml file group
         for xml in xml_paths:
             trig = parsed_VOEvent(xml)
@@ -27,10 +36,11 @@ class test_group_01(TestCase):
                 trigger_id=trig.trig_id,
                 sequence_num=trig.sequence_num,
                 event_type=trig.event_type,
-                ra=trig.ra,
-                dec=trig.dec,
-                ra_hms=Angle(trig.ra, unit=u.deg).to_string(unit=u.hour, sep=':'),
-                dec_dms=Angle(trig.dec, unit=u.deg).to_string(unit=u.deg, sep=':'),
+                # Sent event up so it's always pointing at zenith
+                ra=ra_dec.ra.deg,
+                dec=ra_dec.dec.deg,
+                ra_hms=ra_dec.ra.to_string(unit=u.hour, sep=':'),
+                dec_dms=ra_dec.dec.to_string(unit=u.deg, sep=':'),
                 pos_error=trig.err,
                 ignored=trig.ignore,
                 source_name=trig.source_name,
@@ -41,7 +51,12 @@ class test_group_01(TestCase):
                 swift_rate_signif=trig.swift_rate_signif,
             )
 
+
     def test_trigger_groups(self):
         # Check there are three VOEvents that were grouped as one TriggerEvent
         self.assertEqual(len(VOEvent.objects.all()), 3)
         self.assertEqual(len(TriggerEvent.objects.all()), 1)
+
+    def test_proposal_decision(self):
+        print(f"\n\n!!!!!!!!!!!!!!\n{ProposalDecision.objects.get(id=1).decision_reason}\n!!!!!!!!!!!!!!!\n\n")
+        self.assertEqual(ProposalDecision.objects.get(id=1).decision, 'T')
