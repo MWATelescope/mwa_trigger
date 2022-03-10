0a2b8841dc02cfe29bea4024e34ab859379e4730
NickSwainston <nickaswainston@gmail.com>
2022-03-10 03:15:20 +0000

    For the scheduler to work in production I had to use django_apscheduler in a seperate script to avoid thread blocking issues

--- trigger_webapp/trigger_webapp/settings.py
+++ trigger_webapp/trigger_webapp/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_apscheduler",
     'trigger_app',
 ]
 
@@ -167,3 +168,19 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mwa.trigger@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('GMAIL_APP_PASSWORD')
+
+# Format string for displaying run time timestamps in the Django admin site. The default
+# just adds seconds to the standard Django format, which is useful for displaying the timestamps
+# for jobs that are scheduled to run on intervals of less than one minute.
+#
+# See https://docs.djangoproject.com/en/dev/ref/settings/#datetime-format for format string
+# syntax details.
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
+
+# Maximum run time allowed for jobs that are triggered manually via the Django admin site, which
+# prevents admin site HTTP requests from timing out.
+#
+# Longer running jobs should probably be handed over to a background task processing library
+# that supports multiple background worker processes instead (e.g. Dramatiq, Celery, Django-RQ,
+# etc. See: https://djangopackages.org/grids/g/workers-queues-tasks/ for popular options).
+APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
---
+++ trigger_webapp/twistd_comet_wrapper.py
@@ -0,0 +1,76 @@
+import os
+# Configure settings for project
+# Need to run this before calling models from application!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trigger_webapp.settings')
+import django
+# Import settings
+django.setup()
+from django.conf import settings
+
+from subprocess import Popen, PIPE
+import time
+
+from trigger_app.models import CometLog, Status
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler.jobstores import DjangoJobStore
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+def output_popen_stdout(process):
+    output = process.stdout.readline()
+    logger.debug(f"output: {output}")
+    if output:
+        # New output so send it to the log
+        CometLog.objects.create(log=output.strip().decode())
+    comet_status = Status.objects.get(name='twistd_comet')
+    poll = process.poll()
+    logger.debug(f"poll: {poll}")
+    if poll is None:
+        # Process is still running
+        comet_status.status = 0
+    elif poll == 0:
+        # Finished for some reason
+        comet_status.status = 2
+    else:
+        # Broken
+        comet_status.status = 1
+    comet_status.save()
+
+
+if __name__ == '__main__':
+    # kill unterminated twistd jobs
+    if os.path.exists("/tmp/twistd_comet.pid"):
+        call("kill `cat /tmp/twistd_comet.pid`", shell=True)
+    logger.info("Starting twistd")
+    process = Popen("twistd --pidfile /tmp/twistd_comet.pid -n comet --local-ivo=ivo://hotwired.org/test --remote=voevent.4pisky.org --cmd=upload_xml.py", shell=True, stdout=PIPE)
+    # get initial output right away
+    output_popen_stdout(process)
+    # Add the schedule job
+    scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
+    #scheduler.add_jobstore(DjangoJobStore(), "default")
+
+    scheduler.add_job(
+        output_popen_stdout,
+        trigger=CronTrigger(second="*/59"),  # Every 60 seconds
+        id="output_popen_stdout",
+        max_instances=1,
+        replace_existing=True,
+        kwargs={'process':process},
+    )
+    logger.info("Added job 'output_popen_stdout'.")
+    logger.info("Starting scheduler...")
+    scheduler.start()
+    logger.info("I moved on")
+    logger.info('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(5)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
