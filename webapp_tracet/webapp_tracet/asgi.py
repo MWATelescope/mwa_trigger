a57acf189120ad6350df536131b1fccefe5f7fa1
Nick Swainston <nickaswainston@gmail.com>
2022-07-05 10:08:54 +0800

    Got the rest of the trigger_webapp to webapp_tracet

--- webapp_tracet/webapp_tracet/asgi.py
+++ webapp_tracet/webapp_tracet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for trigger_webapp project.
+ASGI config for webapp_tracet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trigger_webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp_tracet.settings')
 
 application = get_asgi_application()
