c2aa1a6580183d60d773bfc01dc3a4a7f16e4f59
PaulHancock <paul.hancock@curtin.edu.au>
2022-07-19 14:06:22 +0800

    add tracet logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = "figures/TraceT.png"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
