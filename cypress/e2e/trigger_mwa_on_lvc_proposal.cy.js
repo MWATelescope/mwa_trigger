9b244a7d1c46631409ae5f7c20a6e1d2216513bf
Dylan <292206H@curtin.edu.au>
2023-01-24 16:13:11 +0800

    run all cypress tests, seed test data

--- cypress/e2e/trigger_mwa_on_lvc_proposal.cy.js
+++ cypress/e2e/trigger_mwa_on_lvc_proposal.cy.js
@@ -1,13 +1,12 @@
 //./manage.py loaddata trigger_app/test_yamls/mwa_fs_proposal_settings.yaml
 
-describe.skip('webpage loads and connects to the broker', () => {
+describe('webpage loads', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get('.PositiveTransaction').exists
   })
 })
 
-describe.skip(`LVC events are grouped by id with source type, event type, Classification-Terrestrial, 
+describe(`LVC events are grouped by id with source type, event type, Classification-Terrestrial, 
           Properties-HasNS, HasMassGap, Observation-Time, highest_probability_density_sky_location, 
           highest_probability_density_gw`, () => {
   it('upload lvc test event', () => {
@@ -49,7 +48,7 @@ describe.skip(`LVC events are grouped by id with source type, event type, Classi
   })
 })
 
-describe.skip('User can create proposal for MWA observations using LVC events', () => {
+describe('User can create proposal for MWA observations using LVC events', () => {
   it('create and view proposal', () => {
 
     const proposalId = "testMWALVC"
@@ -110,7 +109,7 @@ describe('Early warning LVC events that don\'t trigger the proposal show as igno
   })
 })
 
-describe('Early warning LVC events that trigger the proposal show as observed', () => {
+describe('Early warning LVC events that trigger the proposal show decision outcome', () => {
   it('upload lvc early warning real event and trigger an MWA observation with twilio notifications', () => {
     const graceDBId = "MS44441s"
 
@@ -135,36 +134,6 @@ describe('Early warning LVC events that trigger the proposal show as observed', 
     cy.get("[data-testid='nav-logs']").click()
     cy.get("[data-testid='drop-logs-proposals']").click()
     cy.contains("Above horizon so attempting to observer with MWA_VCS.")
-    cy.wait(5000)
-
-  })
-})
-
-describe.only('Early warning LVC events that trigger the proposal show as observed', () => {
-  it('upload lvc early warning real event and trigger an MWA observation with twilio notifications', () => {
-    const graceDBId = "MS44441s"
-
-    cy.login()
-    cy.visit('/')
-    cy.wait(1000)
-
-    //upload lvc "real" event that we want to trigger on
-    cy.fixture('LVC_example_early_warning_real_promising.txt').then((event1) => {
-      cy.get('[data-testid="nav-testing"]').click({ force: true })
-      cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("MS181101ab", graceDBId)))
-      cy.wait(1000)
-      cy.get("[type='submit']").click()
-      cy.wait(2000)
-    })
-    //proposal result shows event triggered
-    cy.contains(graceDBId).parent('tr').within(() => {
-      cy.get('td').eq(5).contains('GW')
-      // cy.get('td').eq(7).contains('Ignored')
-
-    })
-    cy.get("[data-testid='nav-logs']").click()
-    cy.get("[data-testid='drop-logs-proposals']").click()
-    // cy.contains("The terrestial probability (0.96) is greater than 0.95 so not triggering.")
     cy.wait(5000)
 
   })
