a171c534d00143ef0f74b44a51d2d251b51c6f0b
NickSwainston <nickaswainston@gmail.com>
2023-01-30 05:33:36 +0000

    ignore chime for now

--- webapp_tracet/webapp_tracet/settings.py
+++ webapp_tracet/webapp_tracet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.mwa-trigger.duckdns.org', 'mwa-trigger.duckdns.org', 'www.tracet.duckdns.org', 'tracet.duckdns.org', '146.118.70.58']
 
 # Remote broadcasters we subscribe to for VOEvents
-VOEVENT_REMOTES = ["voevent.4pisky.org","voevent.dc3.com", "chimefrb.physics.mcgill.ca"]
+VOEVENT_REMOTES = ["voevent.4pisky.org","voevent.dc3.com"]#, "chimefrb.physics.mcgill.ca"]
 # TCP connectiong we are whitelisting with to recieve VOEvents
 VOEVENT_TCP = ["196.44.140.214/32","68.169.57.253","50.116.49.68"]
 
