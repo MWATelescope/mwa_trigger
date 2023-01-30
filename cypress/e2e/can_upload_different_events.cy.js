9b9ca0059bdd6b8652437a12b1eba701666f0435
Dylan <292206H@curtin.edu.au>
2023-01-30 11:09:55 +0800

    add some tess for hess

---
+++ cypress/e2e/can_upload_different_events.cy.js
@@ -0,0 +1,43 @@
+
+describe('Test other events can be uploaded', () => {
+    it('upload Antares, Fermi, HESS, SWIFT test events', () => {
+      const graceDBId = "MS33841s"
+  
+      cy.login()
+      cy.visit('/')
+      cy.wait(1000)
+  
+      //upload lvc "real" event that we want to trigger on
+      cy.fixture('Antares_1438351269.txt').then((event1) => {
+        cy.get('[data-testid="nav-testing"]').click({ force: true })
+        cy.get('[class="form-control"]').invoke('val', (event1))
+        cy.wait(1000)
+        cy.get("[type='submit']").click()
+        cy.wait(2000)
+      })
+      //upload lvc "real" event that we want to trigger on
+      cy.fixture('Fermi00.txt').then((event1) => {
+        cy.get('[data-testid="nav-testing"]').click({ force: true })
+        cy.get('[class="form-control"]').invoke('val', event1)
+        cy.wait(1000)
+        cy.get("[type='submit']").click()
+        cy.wait(2000)
+      })
+          //upload lvc "real" event that we want to trigger on
+      cy.fixture('HESS_test_event.txt').then((event1) => {
+        cy.get('[data-testid="nav-testing"]').click({ force: true })
+        cy.get('[class="form-control"]').invoke('val', event1)
+        cy.wait(1000)
+        cy.get("[type='submit']").click()
+        cy.wait(2000)
+      })
+      //upload lvc "real" event that we want to trigger on
+      cy.fixture('SWIFT_2018_03_25.txt').then((event1) => {
+        cy.get('[data-testid="nav-testing"]').click({ force: true })
+        cy.get('[class="form-control"]').invoke('val', event1)
+        cy.wait(1000)
+        cy.get("[type='submit']").click()
+        cy.wait(2000)
+      })
+    })
+  })
