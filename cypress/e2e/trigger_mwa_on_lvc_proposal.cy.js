a71af6a1891fbcf9a940ce296a4e04abe3670ed3
Dylan <292206H@curtin.edu.au>
2023-02-22 13:26:08 +0800

    update tests

--- cypress/e2e/trigger_mwa_on_lvc_proposal.cy.js
+++ cypress/e2e/trigger_mwa_on_lvc_proposal.cy.js
@@ -1,122 +1,145 @@
 //./manage.py loaddata trigger_app/test_yamls/mwa_fs_proposal_settings.yaml
 
-describe('webpage loads', () => {
-  it('passes', () => {
-    cy.visit('/')
-  })
+describe("webpage loads", () => {
+	it("passes", () => {
+		cy.visit("/")
+	})
 })
 
 describe(`LVC events are grouped by id with source type, event type, Classification-Terrestrial, 
           Properties-HasNS, HasMassGap, Observation-Time, highest_probability_density_sky_location, 
           highest_probability_density_gw`, () => {
-  it('upload lvc test event', () => {
-    const graceDBId = "MS553322ab"
+	it("upload lvc test event", () => {
+		const graceDBId = "MS553322ab"
 
-    cy.login()
-    cy.visit('/')
+		cy.login()
+		cy.visit("/")
 
-    // //upload lvc test event
-    cy.fixture('LVC_early_warning_test_event.txt').then((event1) => {
-      cy.get('[data-testid="nav-testing"]').click({ force: true })
-      cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("MS181101ab", graceDBId)))
-      cy.get("[type='submit']").click()
-    })
-    //upload lvc test event
-    cy.fixture('LVC_initial_test_event.txt').then((event1) => {
-      cy.get('[data-testid="nav-testing"]').click({ force: true })
-      cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("MS181101ab", graceDBId)))
-      cy.get("[type='submit']").click()
-    })
-    //events are grouped
-    cy.get('.btn').click()
-    cy.contains(graceDBId).parent('tr').within(() => {
-      cy.get('td > a').eq(0).click()
-    })
-    cy.get('[data-testid="eventgroup"]').find('tr').should('have.length', 3)
-    cy.get('[data-testid="eventgroup"]').find('tr').eq(1)
-      .within(() => {
-        // all searches are automatically rooted to the found tr element
-        cy.get('td').eq(1).contains('GW')
-        cy.get('td').eq(2).contains('LVC')
-        cy.get('td').eq(3).contains('Initial')
-        cy.get('td').eq(4).contains('2018-11-01 22:22:46')
-        cy.get('td').eq(6).contains(graceDBId)
-      })
-  })
+		// //upload lvc test event
+		cy.fixture("LVC_early_warning_test_event.txt").then((event1) => {
+			cy.get('[data-testid="nav-testing"]').click({ force: true })
+			cy.get('[class="form-control"]').invoke(
+				"val",
+				event1.replaceAll("MS181101ab", graceDBId)
+			)
+			cy.get("[type='submit']").click()
+		})
+		//upload lvc test event
+		cy.fixture("LVC_initial_test_event.txt").then((event1) => {
+			cy.get('[data-testid="nav-testing"]').click({ force: true })
+			cy.get('[class="form-control"]').invoke(
+				"val",
+				event1.replaceAll("MS181101ab", graceDBId)
+			)
+			cy.get("[type='submit']").click()
+		})
+		//events are grouped
+		cy.get(".btn").click()
+		cy.contains(graceDBId)
+			.parent("tr")
+			.within(() => {
+				cy.get("td > a").eq(0).click()
+			})
+		cy.get('[data-testid="eventgroup"]').find("tr").should("have.length", 3)
+		cy.get('[data-testid="eventgroup"]')
+			.find("tr")
+			.eq(1)
+			.within(() => {
+				// all searches are automatically rooted to the found tr element
+				cy.get("td").eq(1).contains("GW")
+				cy.get("td").eq(2).contains("LVC")
+				cy.get("td").eq(3).contains("Initial")
+				cy.get("td").eq(4).contains("2018-11-01 22:22:46")
+				cy.get("td").eq(6).contains(graceDBId)
+			})
+	})
 })
 
-describe('User can create proposal for MWA observations using LVC events', () => {
-  it('create and view proposal', () => {
+describe("User can create proposal for MWA observations using LVC events", () => {
+	it("create and view proposal", () => {
+		const proposalId = "testMWALVC"
+		const proposalDescription =
+			"This proposal tests MWA observation LVC triggers"
 
-    const proposalId = "testMWALVC"
-    const proposalDescription = "This proposal tests MWA observation LVC triggers"
+		cy.login()
+		cy.visit("/")
 
-    cy.login()
-    cy.visit('/')
+		cy.get("[data-testid='nav-proposal-settings']").click()
+		cy.get("[data-testid='drop-create-proposal']").click()
+		cy.get("#id_proposal_id").type(proposalId)
+		cy.get("#proposal_description").type(proposalDescription)
+		cy.get("#id_source_type").select("GW")
+		cy.get("#event_telescope").select("LVC")
 
-    cy.get("[data-testid='nav-proposal-settings']").click()
-    cy.get("[data-testid='drop-create-proposal']").click()
-    cy.get("#id_proposal_id").type(proposalId)
-    cy.get("#proposal_description").type(proposalDescription)
-    cy.get("#id_source_type").select('GW')
-    cy.get("#event_telescope").select('LVC')
+		cy.get("#id_telescope").select("MWA_VCS")
+		cy.get("#id_project_id").select("T001")
+		cy.get("#id_testing").check()
+		cy.get("[type='submit']").click()
 
-    cy.get("#id_telescope").select('MWA_VCS')
-    cy.get("#id_project_id").select('T001')
-    cy.get("#id_testing").check()
-    cy.get("[type='submit']").click()
-
-    cy.contains(proposalDescription)
-
-  })
+		cy.contains(proposalDescription)
+	})
 })
 
-describe('Early warning LVC events that don\'t trigger the proposal show as ignored', () => {
-  it('upload lvc early warning real event and get ignored because terrestial is > 95', () => {
-    const graceDBId = "MS43555s"
+describe("Early warning LVC events that don't trigger the proposal show as ignored", () => {
+	it("upload lvc early warning real event and get ignored because terrestial is > 95", () => {
+		const graceDBId = "MS43555s"
 
-    cy.login()
-    cy.visit('/')
+		cy.login()
+		cy.visit("/")
 
-    //upload lvc "real" event that we don't want to trigger on because terrestial > 95
-    cy.fixture('LVC_early_warning_ignore_observation_event.txt').then((event1) => {
-      cy.get('[data-testid="nav-testing"]').click({ force: true })
-      cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("MS181101ab", graceDBId)))
-      cy.get("[type='submit']").click()
-    })
-    //proposal result shows event ignored
-    cy.contains(graceDBId).parent('tr').within(() => {
-      cy.get('td').eq(5).contains('GW')
-      cy.get('td').eq(7).contains('Ignored')
-
-    })
-    cy.get("[data-testid='nav-logs']").click()
-    cy.get("[data-testid='drop-logs-proposals']").click()
-    cy.contains("The PROB_Terre probability (0.96) is greater than 0.95 so not triggering.")
-  })
+		//upload lvc "real" event that we don't want to trigger on because terrestial > 95
+		cy.fixture("LVC_early_warning_ignore_observation_event.txt").then(
+			(event1) => {
+				cy.get('[data-testid="nav-testing"]').click({ force: true })
+				cy.get('[class="form-control"]').invoke(
+					"val",
+					event1.replaceAll("MS181101ab", graceDBId)
+				)
+				cy.get("[type='submit']").click()
+			}
+		)
+		//proposal result shows event ignored
+		cy.contains(graceDBId)
+			.parent("tr")
+			.within(() => {
+				cy.get("td").eq(5).contains("GW")
+				cy.get("td").eq(7).contains("Ignored")
+			})
+		cy.get("[data-testid='nav-logs']").click()
+		cy.get("[data-testid='drop-logs-proposals']").click()
+		cy.contains(
+			"The PROB_Terre probability (0.96) is greater than 0.95 so not triggering."
+		)
+	})
 })
 
-describe('Early warning LVC events that trigger the proposal show decision outcome', () => {
-  it('upload lvc early warning real event and trigger an MWA observation with twilio notifications', () => {
-    const graceDBId = "MS33841s"
+describe("Early warning LVC events that trigger the proposal show decision outcome", () => {
+	it("upload lvc early warning real event and trigger an MWA observation with twilio notifications", () => {
+		const graceDBId = "MS33841s"
 
-    cy.login()
-    cy.visit('/')
+		cy.login()
+		cy.visit("/")
 
-    //upload lvc "real" event that we want to trigger on
-    cy.fixture('LVC_early_warning_promising_observation_event.txt').then((event1) => {
-      cy.get('[data-testid="nav-testing"]').click({ force: true })
-      cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("MS181101ab", graceDBId)))
-      cy.get("[type='submit']").click()
-    })
-    //proposal result shows event triggered
-    cy.contains(graceDBId).parent('tr').within(() => {
-      cy.get('td').eq(5).contains('GW')
-      cy.get('td').eq(7).contains('Triggered')
-
-    })
-    cy.get("[data-testid='nav-logs']").click()
-    cy.get("[data-testid='drop-logs-proposals']").click()
-    cy.contains("Above horizon so attempting to observer with MWA_VCS.")
-  })
+		//upload lvc "real" event that we want to trigger on
+		cy.fixture("LVC_early_warning_promising_observation_event.txt").then(
+			(event1) => {
+				cy.get('[data-testid="nav-testing"]').click({ force: true })
+				cy.get('[class="form-control"]').invoke(
+					"val",
+					event1.replaceAll("MS181101ab", graceDBId)
+				)
+				cy.get("[type='submit']").click()
+			}
+		)
+		//proposal result shows event triggered
+		cy.contains(graceDBId)
+			.parent("tr")
+			.within(() => {
+				cy.get("td").eq(5).contains("GW")
+				cy.get("td").eq(7).contains("Triggered")
+			})
+		cy.get("[data-testid='nav-logs']").click()
+		cy.get("[data-testid='drop-logs-proposals']").click()
+		cy.contains("Above horizon so attempting to observe with MWA_VCS.")
+	})
 })
--- cypress/e2e/trigger_on_hess_events.cy.js
+++ cypress/e2e/trigger_on_hess_events.cy.js
@@ -1,91 +1,102 @@
+describe("Test HESS events will group with swift", () => {
+	it("create HESS and swift events, check that they group", () => {
+		cy.login()
+		cy.visit("/")
 
-describe('Test HESS events will group with swift', () => {
-    it('create HESS and swift events, check that they group', () => {
-
-        cy.login()
-        cy.visit('/')
-
-        const swiftId = "144329"
-        // upload HESS test event
-        cy.fixture('HESS_promising_test_event.txt').then((event1) => {
-            cy.get('[data-testid="nav-testing"]').click({ force: true })
-            cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("1102329", swiftId)))
-            cy.get("[type='submit']").click()
-        })
-        // upload SWIFT test event
-        cy.fixture('SWIFT_promising_test_event.txt').then((event1) => {
-            cy.get('[data-testid="nav-testing"]').click({ force: true })
-            cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("817564", swiftId)))
-            cy.get("[type='submit']").click()
-        })
-        // events are grouped
-        cy.get('.btn').click()
-        cy.contains(swiftId).parent('tr').within(() => {
-            cy.get('td > a').eq(0).click()
-        })
-        cy.get('[data-testid="eventgroup"]').find('tr').should('have.length', 3)
-        cy.get('[data-testid="eventgroup"]').find('tr').eq(1)
-            .within(() => {
-                // all searches are automatically rooted to the found tr element
-                cy.get('td').eq(1).contains('GRB')
-                cy.get('td').eq(6).contains(swiftId)
-            })
-    })
-
+		const swiftId = "144329"
+		// upload HESS test event
+		cy.fixture("HESS_promising_test_event.txt").then((event1) => {
+			cy.get('[data-testid="nav-testing"]').click({ force: true })
+			cy.get('[class="form-control"]').invoke(
+				"val",
+				event1.replaceAll("1102329", swiftId)
+			)
+			cy.get("[type='submit']").click()
+		})
+		// upload SWIFT test event
+		cy.fixture("SWIFT_promising_test_event.txt").then((event1) => {
+			cy.get('[data-testid="nav-testing"]').click({ force: true })
+			cy.get('[class="form-control"]').invoke(
+				"val",
+				event1.replaceAll("817564", swiftId)
+			)
+			cy.get("[type='submit']").click()
+		})
+		// events are grouped
+		cy.get(".btn").click()
+		cy.contains(swiftId)
+			.parent("tr")
+			.within(() => {
+				cy.get("td > a").eq(0).click()
+			})
+		cy.get('[data-testid="eventgroup"]').find("tr").should("have.length", 3)
+		cy.get('[data-testid="eventgroup"]')
+			.find("tr")
+			.eq(1)
+			.within(() => {
+				// all searches are automatically rooted to the found tr element
+				cy.get("td").eq(1).contains("GRB")
+				cy.get("td").eq(6).contains(swiftId)
+			})
+	})
 })
 
+describe("Test can create proposals to do observations off HESS events", () => {
+	it("create HESS trigger proposal", () => {
+		const proposalId = "testMWAHESS"
+		const proposalDescription =
+			"This proposal tests MWA observation HESS triggers"
 
-describe('Test can create proposals to do observations off HESS events', () => {
-    it('create HESS trigger proposal', () => {
+		cy.login()
+		cy.visit("/")
 
-        const proposalId = "testMWAHESS"
-        const proposalDescription = "This proposal tests MWA observation HESS triggers"
+		cy.get("[data-testid='nav-proposal-settings']").click()
+		cy.get("[data-testid='drop-create-proposal']").click()
+		cy.get("#id_proposal_id").type(proposalId)
+		cy.get("#proposal_description").type(proposalDescription)
+		cy.get("#id_source_type").select("GRB")
+		cy.get("#event_telescope").select("HESS")
 
-        cy.login()
-        cy.visit('/')
+		// Source options
+		cy.get("#id_event_any_duration").click()
 
-        cy.get("[data-testid='nav-proposal-settings']").click()
-        cy.get("[data-testid='drop-create-proposal']").click()
-        cy.get("#id_proposal_id").type(proposalId)
-        cy.get("#proposal_description").type(proposalDescription)
-        cy.get("#id_source_type").select('GRB')
-        cy.get("#event_telescope").select('HESS')
+		cy.get("#id_telescope").select("MWA_VCS")
+		cy.get("#id_project_id").select("T001")
+		cy.get("#id_testing").check()
+		cy.get("[type='submit']").click()
 
-        // Source options
-        cy.get("#id_event_any_duration").click()
-
-        cy.get("#id_telescope").select('MWA_VCS')
-        cy.get("#id_project_id").select('T001')
-        cy.get("#id_testing").check()
-        cy.get("[type='submit']").click()
-
-        cy.contains(proposalDescription)
-    })
+		cy.contains(proposalDescription)
+	})
 })
 
-describe('HESS events that trigger the proposal show decision outcome', () => {
-    it('upload HESS real event and trigger an MWA observation with twilio notifications', () => {
-        cy.login()
-        cy.visit('/')
+describe("HESS events that trigger the proposal show decision outcome", () => {
+	it("upload HESS real event and trigger an MWA observation with twilio notifications", () => {
+		cy.login()
+		cy.visit("/")
 
-        const hessId = '222222'
+		const hessId = "222222"
 
-        //upload lvc "real" event that we want to trigger on
-        cy.fixture('HESS_promising_observation_event.txt').then((event1) => {
-            cy.get('[data-testid="nav-testing"]').click({ force: true })
-            cy.get('[class="form-control"]').invoke('val', (event1.replaceAll("1102329", hessId)))
-            cy.get("[type='submit']").click()
-        })
-        //proposal result shows event triggered
-        cy.contains(hessId).parent('tr').within(() => {
-            cy.get('td').eq(4).contains('GRB')
-            cy.get('td').eq(2).contains('HESS')
-        })
-        cy.get("[data-testid='nav-logs']").click()
-        cy.get("[data-testid='drop-logs-proposals']").click()
-        cy.get(".fl-table > tbody:nth-child(2) > tr:nth-child(1)").within(() => {
-            cy.get('td').eq(6).contains('HESS rate significance is')
-            cy.get('td').eq(6).contains('Above horizon so attempting to observe')
-        })
-    })
+		//upload lvc "real" event that we want to trigger on
+		cy.fixture("HESS_promising_observation_event.txt").then((event1) => {
+			cy.get('[data-testid="nav-testing"]').click({ force: true })
+			cy.get('[class="form-control"]').invoke(
+				"val",
+				event1.replaceAll("1102329", hessId)
+			)
+			cy.get("[type='submit']").click()
+		})
+		//proposal result shows event triggered
+		cy.contains(hessId)
+			.parent("tr")
+			.within(() => {
+				cy.get("td").eq(4).contains("GRB")
+				cy.get("td").eq(2).contains("HESS")
+			})
+		cy.get("[data-testid='nav-logs']").click()
+		cy.get("[data-testid='drop-logs-proposals']").click()
+		cy.get(".fl-table > tbody:nth-child(2) > tr:nth-child(1)").within(() => {
+			cy.get("td").eq(9).contains("HESS rate significance is")
+			cy.get("td").eq(9).contains("Above horizon so attempting to observe")
+		})
+	})
 })
